a1ec2758858201466861a7d6588f91afa543d00c
Yannick Scherer <yannick.scherer@stylefruits.de>
2015-09-17 18:13:22 +0200

    Version 0.1.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xsc/pem-reader "0.1.1-SNAPSHOT"
+(defproject xsc/pem-reader "0.1.1"
   :description "A lightweight PEM reader, retrieving keys as byte arrays."
   :url "https://github.com/xsc/pem-reader"
   :license {:name "MIT License"
