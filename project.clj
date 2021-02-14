2a253b1c7151285f19e93b4e470e09439639f800
Yannick Scherer <yannick.scherer@futurice.com>
2021-02-14 15:01:01 +0100

    Use Github Actions and Kaocha

--- project.clj
+++ project.clj
@@ -8,4 +8,19 @@
             :comment "MIT License"}
   :dependencies [[org.clojure/clojure "1.10.2" :scope "provided"]
                  [org.clojure/data.codec "0.1.1"]]
+  :profiles {:dev
+             {:global-vars {*warn-on-reflection* true}}
+             :kaocha
+             {:dependencies [[lambdaisland/kaocha "1.0.732"
+                              :exclusions [org.clojure/spec.alpha]]
+                             [lambdaisland/kaocha-cloverage "1.0.75"]]}
+             :ci
+             [:kaocha
+              {:global-vars {*warn-on-reflection* false}}]}
+  :aliases {"kaocha"    ["with-profile" "+kaocha" "run" "-m" "kaocha.runner"]
+            "ci"        ["with-profile" "+ci" "run" "-m" "kaocha.runner"
+                         "--reporter" "documentation"
+                         "--plugin"   "cloverage"
+                         "--codecov"
+                         "--no-cov-html"]}
   :pedantic? :abort)
